51bd48d4486ce700697161aaf136a09aa659f697
FlashOnFire_ <flashonfire.dev@gmail.com>
2025-07-25 23:11:45 +0200

    Updated options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -3,13 +3,11 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Line number / relative line numbers
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
+-- Allow usage of mouse in all modes
 vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
@@ -17,11 +15,11 @@ vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
--- Enable break indent
+-- Indent every line that results from line wrapping to form a block
+-- rather than indenting only the first line
 vim.o.breakindent = true
 
 -- Save undo history
@@ -31,7 +29,7 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
--- Keep signcolumn on by default
+-- Always show sign column
 vim.o.signcolumn = 'yes'
 
 -- Decrease update time
@@ -45,20 +43,13 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+-- Preview substitutions live
+vim.o.inccommand = 'nosplit'
 
--- Show which line your cursor is on
+-- Show the current line in a slightly different color
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
@@ -66,7 +57,10 @@ vim.o.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Indents
+vim.o.autoindent = true
+vim.o.smartindent = true
 
 -- vim: ts=2 sts=2 sw=2 et
