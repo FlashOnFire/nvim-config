252ad446695a609e376d3b66b975234ce98c7751
FlashOnFire_ <flashonfire.dev@gmail.com>
2025-07-28 00:00:57 +0200

    Added Monokai-Pro theme

---
+++ lua/custom/plugins/monokai-pro.lua
@@ -0,0 +1,21 @@
+-- Monokai theme
+return {
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'loctvl842/monokai-pro.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('monokai-pro').setup({})
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme('monokai-pro')
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -15,7 +15,7 @@ require('lazy').setup({
 
   require('kickstart.plugins.blink-cmp'),
 
-  require('kickstart.plugins.tokyonight'),
+  -- require('kickstart.plugins.tokyonight'),
 
   require('kickstart.plugins.todo-comments'),
 
