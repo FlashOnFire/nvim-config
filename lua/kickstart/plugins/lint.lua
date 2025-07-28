f349d76e1cb3af2834570246fe838b8597366247
FlashOnFire_ <flashonfire.dev@gmail.com>
2025-07-28 23:13:17 +0200

    Removed markdown lint (too picky/annoying and i am too lazy to configure it properly)

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -8,7 +8,7 @@ return {
     config = function()
       local lint = require('lint')
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         cpp = { 'clang-tidy' },
         rust = { 'clippy' },
         sh = { 'shellcheck' },
