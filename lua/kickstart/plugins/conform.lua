b7ed0bc009bbdb32829adb74c7bee3db7bb81012
FlashOnFire_ <flashonfire.dev@gmail.com>
2025-09-16 20:36:48 +0200

    Disable CMake auto format

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -18,7 +18,7 @@ return {
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, cmake = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
