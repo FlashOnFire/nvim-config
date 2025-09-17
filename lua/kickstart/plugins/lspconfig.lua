42e5810179808467c4ff35dfb3e2fcd1e037c2fa
FlashOnFire_ <flashonfire.dev@gmail.com>
2025-09-17 20:40:12 +0200

    Remove clutter

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -268,8 +268,6 @@ return {
               },
             },
           },
-          -- gopls = {},
-          -- pyright = {},
           rust_analyzer = {
             settings = {
               ['rust-analyzer'] = {
@@ -283,13 +281,6 @@ return {
           csharp_ls = {},
           ruff = {},
           -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-          --
-          -- Some languages (like typescript) have entire language plugins that can be useful:
-          --    https://github.com/pmizio/typescript-tools.nvim
-          --
-          -- But for many setups, the LSP (`ts_ls`) will work just fine
-          -- ts_ls = {},
-          --
           lua_ls = {
             -- cmd = { ... },
             -- filetypes = { ... },
@@ -314,19 +305,6 @@ return {
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers.mason or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Lua formatter
