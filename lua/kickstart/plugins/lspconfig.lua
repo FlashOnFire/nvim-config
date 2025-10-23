0f16d23d7ed5946c17feab2fb8052bd0c30215ab
FlashOnFire_ <flashonfire.dev@gmail.com>
2025-10-23 21:25:03 +0200

    Added custom mason registry (to provide nixfmt 1.1.0)

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -27,7 +27,12 @@ return {
         ---@module 'mason.settings'
         ---@type MasonSettings
         ---@diagnostic disable-next-line: missing-fields
-        opts = {},
+        opts = {
+          registries = {
+            'github:FlashOnFire/mason-registry',
+            'github:mason-org/mason-registry',
+          },
+        },
       },
       'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
