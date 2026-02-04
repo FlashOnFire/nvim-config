46a6352a86f4f212e0c72eba2caec8c05798d64a
FlashOnFire_ <flashonfire.dev@gmail.com>
2026-02-04 22:22:28 +0100

    fix: unbreak leap.nvim after update

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,20 +3,50 @@
 return {
   {
     'https://codeberg.org/andyg/leap.nvim',
-    enabled = true,
-    keys = {
-      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
-      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
-      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
-    },
-    config = function(_, opts)
-      local leap = require('leap')
-      for k, v in pairs(opts) do
-        leap.opts[k] = v
+    config = function()
+      -- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+      -- vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+
+      local clever_s = require('leap.user').with_traversal_keys('s', 'S')
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', function() require('leap').leap({ opts = clever_s }) end, { desc = 'Leap Forward' })
+      vim.keymap.set({ 'n', 'x', 'o' }, 'S', function() require('leap').leap({ backward = true, opts = clever_s }, { desc = 'Leap Backwards' }) end)
+
+      -- Clever f/F/t/T binds (stolen from the plugin FAQ)
+
+      -- Return an argument table for `leap()`, tailored for f/t-motions.
+      local function as_ft(key_specific_args)
+        local common_args = {
+          inputlen = 1,
+          inclusive = true,
+          -- To limit search scope to the current line:
+          -- pattern = function (pat) return '\\%.l'..pat end,
+          opts = {
+            labels = '', -- force autojump
+            safe_labels = vim.fn.mode(1):match('[no]') and '' or nil, -- [1]
+          },
+        }
+        return vim.tbl_deep_extend('keep', common_args, key_specific_args)
       end
-      leap.add_default_mappings(true)
-      vim.keymap.del({ 'x', 'o' }, 'x')
-      vim.keymap.del({ 'x', 'o' }, 'X')
+
+      local clever = require('leap.user').with_traversal_keys -- [2]
+      local clever_f = clever('f', 'F')
+      local clever_t = clever('t', 'T')
+
+      for key, key_specific_args in pairs({
+        f = { opts = clever_f },
+        F = { backward = true, opts = clever_f },
+        t = { offset = -1, opts = clever_t },
+        T = { backward = true, offset = 1, opts = clever_t },
+      }) do
+        vim.keymap.set({ 'n', 'x', 'o' }, key, function() require('leap').leap(as_ft(key_specific_args)) end)
+      end
+
+      -- [1] Match the modes here for which you don't want to use labels
+      --     (`:h mode()`, `:h lua-pattern`).
+      -- [2] This helper function makes it easier to set "clever-f"-like
+      --     functionality (https://github.com/rhysd/clever-f.vim), returning
+      --     an `opts` table derived from the defaults, where the given keys
+      --     are added to `keys.next_target` and `keys.prev_target`
     end,
   },
 }
