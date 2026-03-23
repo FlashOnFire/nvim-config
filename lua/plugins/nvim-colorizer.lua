9d57701f0d9754f34db07487fbbdae365e29e5a6
FlashOnFire_ <flashonfire.dev@gmail.com>
2026-03-23 10:31:35 +0100

    fix: use new nvim-colorizer config format

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -17,10 +17,14 @@ return {
       filetypes = {
         '*',
       },
-      user_default_options = {
-        css = true,
-        css_fn = true,
-        mode = 'virtualtext',
+      options = {
+        parsers = {
+          css = true,
+          css_fn = true,
+        },
+        display = {
+          mode = 'virtualtext',
+        },
       },
     },
   },
